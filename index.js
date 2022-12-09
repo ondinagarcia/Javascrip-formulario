ef442304759c921b2fc4f3b16c519dfa05e692be
Your Name <you@example.com>
2022-12-09 10:52:08 -0600

    se creo la carpeta de assets

---
+++ index.js
@@ -0,0 +1,8 @@
+// Constante de contenedor del contenido inyeectado con JS
+const contenedor = document.getElementById("Contenedor");
+
+
+// crear event listener para la forma.
+
+//evitar la acccion por defecto
+e.preventde
